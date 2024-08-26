d0838151b44d2e95447b92396d45518a3b77fc18
Alex <Alex.xu@allsaintshigh.org>
2024-08-26 14:58:04 -0500

    Initial comit - Day 1 notes

---
+++ src/ACSII.java
@@ -0,0 +1,21 @@
+public class ACSII {
+    public static void main(String[] args) {
+        System.out.println("             ************");
+        System.out.println("         ***               ***       ");
+        System.out.println("       ***                   ***        ");
+        System.out.println("    ***                        ***");
+        System.out.println("   ***                          ***  ");
+        System.out.println("  ***     ******      *******     *** ");
+        System.out.println(" ***                               ***    ");
+        System.out.println("***                                ***    ");
+        System.out.println("***                                ***     ");
+        System.out.println("***       **                **     ***     ");
+        System.out.println(" ***        **            **       ***   ");
+        System.out.println(" ***          **        **         ***  ");
+        System.out.println("   ***           ******          ***   ");
+        System.out.println("    ***                       *** ");
+        System.out.println("     ***                     ***   ");
+        System.out.println("        ***  *****************  ");
+
+    }
+}
